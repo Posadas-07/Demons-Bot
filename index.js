dad2f795b3f1b3ee6db993404d8484b8826ea64f
Posadasx7 <posadashne@gmail.com>
2025-09-12 12:11:36 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ global.prefixes = defaultPrefixes;
 
 // 🧑‍💼 Owners desde owner.json
 const ownerPath = "./owner.json";
-if (!fs.existsSync(ownerPath)) fs.writeFileSync(ownerPath, JSON.stringify([["15167096032"]], null, 2));
+if (!fs.existsSync(ownerPath)) fs.writeFileSync(ownerPath, JSON.stringify([["50493374445"]], null, 2));
 global.owner = JSON.parse(fs.readFileSync(ownerPath));
 
 // 📂 Cargar plugins
