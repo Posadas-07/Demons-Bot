df0c733b9e0d0a6bbcaf59fa3340ad83dcefe085
Posadasx7 <posadashne@gmail.com>
2025-09-15 07:39:46 -0600

    Crear vegito.js

---
+++ plugins/vegito.js
@@ -0,0 +1,17 @@
+const handler = async (msg, { conn }) => {
+  const chatId = msg.key.remoteJid;
+
+  const videoUrl = "https://cdn.russellxz.click/a91047df.mp4"; // 🎥 Video con sonido
+
+  await conn.sendMessage(chatId, {
+    video: { url: videoUrl },
+    caption: "AQUU TIENES TÚ VEGITO..."
+  }, { quoted: msg });
+
+  await conn.sendMessage(chatId, {
+    react: { text: "💀", key: msg.key }
+  });
+};
+
+handler.command = ["vegito"];
+module.exports = handler;
