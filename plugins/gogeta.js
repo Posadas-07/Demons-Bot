fba32b5eee584ef00ef21059ca3cf449971c4b07
Posadasx7 <posadashne@gmail.com>
2025-09-15 07:42:53 -0600

    Crear gogeta.js

---
+++ plugins/gogeta.js
@@ -0,0 +1,17 @@
+const handler = async (msg, { conn }) => {
+  const chatId = msg.key.remoteJid;
+
+  const videoUrl = "https://cdn.russellxz.click/398cff97.mp4"; // 🎥 Video con sonido
+
+  await conn.sendMessage(chatId, {
+    video: { url: videoUrl },
+    caption: "AQUI TIENES TÚ GOGETA..."
+  }, { quoted: msg });
+
+  await conn.sendMessage(chatId, {
+    react: { text: "💀", key: msg.key }
+  });
+};
+
+handler.command = ["gogeta"];
+module.exports = handler;
