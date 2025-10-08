ca382ee0a4f2c60300db083b4c9546eca19da453
Posadas-07 <elderposadas028@gmail.com>
2025-10-08 10:26:13 -0600

    Actualizar Rest.js

--- plugins/pluginsowner/Rest.js
+++ plugins/pluginsowner/Rest.js
@@ -24,7 +24,7 @@ const handler = async (msg, { conn }) => {
 
   // Mensaje de aviso
   await conn.sendMessage(chatId, {
-    text: "🔄 *Suki Bot se reiniciará en unos segundos...*"
+    text: "*Demons-Bot se reiniciará espera unos segundos...*"
   }, { quoted: msg });
 
   // Guardar chat para notificar luego
